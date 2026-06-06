42682d7316b0b9aa570226f6e80ea89fb18a390a
GenaroScrocca <genaroscroccacardin@gmail.com>
2026-06-06 16:58:26 -0300

    Inicializa el proyecto Spring Boot

---
+++ src/test/java/com/integrador/minibooking/MiniBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.minibooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
